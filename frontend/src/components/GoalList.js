c63353206a28544f1f011e7f8146ca325cd2b51c
Agostino <agostinoscholes572@gmail.com>
2025-07-21 20:49:10 +0300

    updated the GoalList.js and exported it

---
+++ frontend/src/components/GoalList.js
@@ -0,0 +1,22 @@
+import React from "react";
+import GoalCard from "./GoalCard";
+
+function GoalList({ goals, onDeleteGoal, onUpdateGoal }) {
+    return (
+        <section>
+            <h2>Your Goals</h2>
+            <div className="goal-list">
+                {goals.map((goal) => (
+                    <GoalCard
+                        key={goal.id}
+                        goal={goal}
+                        onDelete={onDeleteGoal}
+                        onUpdate={onUpdateGoal}
+                    />
+                ))}
+            </div>
+        </section>
+    );
+}
+
+export default GoalList;
